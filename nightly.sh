dd176bb090ec4b23391d700cbe6c74a3d87c51d5
Charles Severance <csev@umich.edu>
2014-12-30 16:09:37 -0500

    More tweaking.

--- nightly.sh
+++ nightly.sh
@@ -3,6 +3,9 @@ source config-dist.sh
 
 # sh nightly.sh 2>&1 | tee /tmp/nightly-`date '+%Y-%m-%dT%H:%M'`.out
 
+echo "Starting nightly build process"
+date
+
 # rm -rf ~/.m2/repository/org/sakaiproject
 rm -rf trunk
 sh na.sh
