dbbd9c1afd53f600495eeb5ebd7bda8524b01d7e
Charles Severance <csev@umich.edu>
2014-12-30 15:42:36 -0500

    Perhaps first feature complete version.

--- nightly.sh
+++ nightly.sh
@@ -1,9 +1,17 @@
 #! /bin/sh
-source condig-dist.sh
+source config-dist.sh
+
+# sh nightly.sh 2>&1 | tee /tmp/nightly-`date '+%Y-%m-%dT%H:%M'`.out
 
 # rm -rf ~/.m2/repository/org/sakaiproject
 rm -rf trunk
-sh db.sh
 sh na.sh
 sh co.sh
+
+sh stop.sh
+rm -f $LOG_DIRECTORY/*
+sh db.sh
+
 sh qmv.sh
+sh start.sh
+
