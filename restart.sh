82e9a4ab6143bd8ce84c33f6213bad8ecd05cd86
Charles Severance <csev@umich.edu>
2016-02-01 14:15:56 -0500

    Add the restart.sh script

---
+++ restart.sh
@@ -0,0 +1,11 @@
+! /bin/bash
+if [ "$BASH" = "" ] ;then echo "Please run with bash"; exit 1; fi
+source config-dist.sh
+if [ "$PORT" == "" ]; then
+    echo "Bad configuration or wrong shell";
+    exit
+fi
+
+bash stop.sh
+bash start.sh
+bash tail.sh
