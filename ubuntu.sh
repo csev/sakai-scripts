ee0fd92e7c363d56e663e257d22aee27f356ef88
Charles Severance <csev@umich.edu>
2021-09-29 12:05:35 -0400

    Make changes

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,10 +6,11 @@ if [ "$BASH" = "" ] ;then echo "Please run with bash"; exit 1; fi
 # in the first place
 
 apt-get update
-apt-get -y install git curl unzip python-software-properties 
+apt-get -y install git curl unzip zip
 
 # Install sdkman
 curl -s "https://get.sdkman.io" | bash
+source "/root/.sdkman/bin/sdkman-init.sh"
 
 sdk install java 11.0.12-tem
 sdk install maven
@@ -18,8 +19,7 @@ sdk install maven
 # Make the password root - or override the root password in 
 # config.sh - a copied and edited version of config-dist.php
 apt-get -y install mariadb-server
-apt-get -y install mysql-client
-apt-get -y install libmysql-java
+apt-get -y install mariadb-client
 
 # for chrome headless you need this
 apt-get install -y libgbm-dev libxkbcommon-x11-0 libgtk-3-0
