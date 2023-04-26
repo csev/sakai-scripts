b7a213f7a4e07c10dfcb33243a01aeb1ac8692e0
Charles Severance <csev@umich.edu>
2023-04-26 11:03:37 -0400

    Progress.

--- wraptest.sh
+++ wraptest.sh
@@ -1,11 +1,12 @@
 date
-echo "Hello one"
+echo "Hello world"
 
+echo "Sleeping for 10 seconds to test buffer flushing in shell scripts..."
 sleep 5
-
-echo "Hello two"
+echo "Back from sleep"
+echo "Back from sleep2"
 
 echo "The next line should be a stderr..."
 lksdljkjds lkj df
 
-echo "Hello three"
+echo "All done"
