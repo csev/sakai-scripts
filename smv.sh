7decb29277c01cd8bc12937cd000f493bfdfe901
Charles Severance <csev@umich.edu>
2024-08-20 18:32:21 -0400

    Make smv.sh smarter

--- smv.sh
+++ smv.sh
@@ -21,6 +21,22 @@ else
 fi
 cd $startwd
 
+
+if [ -e "pom.xml" ]; then
+    x=`sed -n 's:.*<packaging>\(.*\)</packaging>.*:\1:p' pom.xml`
+    if [ "$x" = "war" ]; then
+        packaging="This folder builds a war file, you should not have to restart your Tomcat"
+    elif [ "$x" = "jar" ]; then
+        packaging="This folder builds a jar file, you probably will need to restart your Tomcat"
+    elif [ "$x" = "sakai-component" ]; then
+        packaging="This folder builds a Sakai service, it requires a restart of your Tomcat"
+    else
+        packaging="You probably need to restart your Tomcat after building this folder"
+    fi
+else
+    packaging="You probably need to restart your Tomcat after building this folder"
+fi
+
 export MAVEN_OPTS='-Xms512m -Xmx1024m -Djava.util.Arrays.useLegacyMergeSort=true'
 
 echo "Running these commands:"
@@ -32,5 +48,12 @@ echo " "
 echo "or "
 echo " "
 echo mvn -Dmaven.tomcat.home=$tomcatdir -Dmaven.test.skip=true clean install sakai:deploy
+echo " "
+echo $packaging
+echo
 
 mvn -Dmaven.tomcat.home=$tomcatdir clean install sakai:deploy
+
+echo " "
+echo $packaging
+echo
